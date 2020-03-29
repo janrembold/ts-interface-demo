c57bce856fb74132d855446eab849cca8bed7b11
janrembold <janrembold@gmx.net>
2020-03-29 13:21:55 +0200

    compile working example

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var test = {
     title: "foo",
     valueA: "bar"
 };
+// works fine
+console.log(test.valueA);
+// Results in: index.ts:20:16 - error TS2339: Property 'valueA' does not exist on type 'Combined'.
 var title = test.title, _a = test.valueA, valueA = _a === void 0 ? "" : _a;
-// index.ts:20:16 - error TS2339: Property 'valueA' does not exist on type 'Combined'.
