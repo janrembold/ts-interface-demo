c479dfe034a3749fcbaaf16cb89275a13abb2c71
janrembold <janrembold@gmx.net>
2020-03-29 12:52:11 +0200

    remove index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var test = {
-    title: "foo",
-    valueA: "bar"
-};
-var title = test.title, _a = test.valueA, valueA = _a === void 0 ? "" : _a;
-// Property 'bar' does not exist on type 'IFooTypes'. TS2339
